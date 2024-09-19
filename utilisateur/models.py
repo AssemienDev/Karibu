0c787aa5e961fca2a5315024c38780d5b1aff353
AssemienDev <139700557+AssemienDev@users.noreply.github.com>
2024-09-19 10:20:32 +0000

    Travail dans la partie utilisateur 100%

--- utilisateur/models.py
+++ utilisateur/models.py
@@ -15,14 +15,15 @@ class Espace(models.Model):
     descriptionGarage = models.TextField(blank=True, null=True)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
 
+
 # Modèle pour les suites
 class Suite(models.Model):
-    numeroChambre = models.CharField(max_length=10, blank=True, null=True)
-    photo1 = models.ImageField(upload_to='document/suites/images/', blank=True, null=True)
+    numeroChambre = models.CharField(max_length=10, unique=True)
+    photo1 = models.ImageField(upload_to='document/suites/images/', blank=False, null=False)
     photo2 = models.ImageField(upload_to='document/suites/images/', blank=True, null=True)
     photo3 = models.ImageField(upload_to='document/suites/images/', blank=True, null=True)
     photo4 = models.ImageField(upload_to='document/suites/images/', blank=True, null=True)
-    video1 = models.FileField(upload_to='document/suites/videos/', blank=True, null=True)
+    video1 = models.FileField(upload_to='document/suites/videos/', blank=False, null=False)
     video2 = models.FileField(upload_to='document/suites/videos/', blank=True, null=True)
     video3 = models.FileField(upload_to='document/suites/videos/', blank=True, null=True)
     description = models.TextField()
@@ -34,17 +35,17 @@ class Suite(models.Model):
         ('occupe', 'Occupé'),
     ]
 
-    
     statutChambre = models.CharField(max_length=10, choices=STATUT_CHOICES, default='libre')
+
 
 # Modèle pour les chambres climatisées
 class ChambreClimatisee(models.Model):
-    numeroChambre = models.CharField(max_length=10, blank=True, null=True)
-    photo1 = models.ImageField(upload_to='document/chambres_climatisees/images/', blank=True, null=True)
+    numeroChambre = models.CharField(max_length=10, unique=True)
+    photo1 = models.ImageField(upload_to='document/chambres_climatisees/images/', blank=False, null=False)
     photo2 = models.ImageField(upload_to='document/chambres_climatisees/images/', blank=True, null=True)
     photo3 = models.ImageField(upload_to='document/chambres_climatisees/images/', blank=True, null=True)
     photo4 = models.ImageField(upload_to='document/chambres_climatisees/images/', blank=True, null=True)
-    video1 = models.FileField(upload_to='document/chambres_climatisees/videos/', blank=True, null=True)
+    video1 = models.FileField(upload_to='document/chambres_climatisees/videos/', blank=False, null=False)
     video2 = models.FileField(upload_to='document/chambres_climatisees/videos/', blank=True, null=True)
     video3 = models.FileField(upload_to='document/chambres_climatisees/videos/', blank=True, null=True)
     description = models.TextField()
@@ -55,17 +56,17 @@ class ChambreClimatisee(models.Model):
         ('occupe', 'Occupé'),
     ]
 
-  
     statutChambre = models.CharField(max_length=10, choices=STATUT_CHOICES, default='libre')
+
 
 # Modèle pour les chambres ventilées
 class ChambreVentilee(models.Model):
-    numeroChambre = models.CharField(max_length=10, blank=True, null=True)
-    photo1 = models.ImageField(upload_to='document/chambres_ventilees/images/', blank=True, null=True)
+    numeroChambre = models.CharField(max_length=10, unique=True)
+    photo1 = models.ImageField(upload_to='document/chambres_ventilees/images/', blank=False, null=False)
     photo2 = models.ImageField(upload_to='document/chambres_ventilees/images/', blank=True, null=True)
     photo3 = models.ImageField(upload_to='document/chambres_ventilees/images/', blank=True, null=True)
     photo4 = models.ImageField(upload_to='document/chambres_ventilees/images/', blank=True, null=True)
-    video1 = models.FileField(upload_to='document/chambres_ventilees/videos/', blank=True, null=True)
+    video1 = models.FileField(upload_to='document/chambres_ventilees/videos/', blank=False, null=False)
     video2 = models.FileField(upload_to='document/chambres_ventilees/videos/', blank=True, null=True)
     video3 = models.FileField(upload_to='document/chambres_ventilees/videos/', blank=True, null=True)
     description = models.TextField()
@@ -77,7 +78,6 @@ class ChambreVentilee(models.Model):
         ('occupe', 'Occupé'),
     ]
 
-    
     statutChambre = models.CharField(max_length=10, choices=STATUT_CHOICES, default='libre')
 
 
@@ -112,7 +112,7 @@ class Chambre(models.Model):
     chambre_climatisee = models.OneToOneField(ChambreClimatisee, on_delete=models.CASCADE, null=True, blank=True)
     chambre_ventilee = models.OneToOneField(ChambreVentilee, on_delete=models.CASCADE, null=True, blank=True)
     suite = models.OneToOneField(Suite, on_delete=models.CASCADE, null=True, blank=True)
-    statutChambre = models.CharField(max_length=20,null=True, blank=True)
+    statutChambre = models.CharField(max_length=20, null=True, blank=True)
 
 
 # Modèle pour les administrateurs
